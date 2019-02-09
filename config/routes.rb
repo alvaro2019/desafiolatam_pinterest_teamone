104fb05ab6593398d1dc2f15f032fefbb5e70654
jzapata18 <john.jzapata18@gmail.com>
2019-02-09 11:40:39 -0300

    system authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/index'
+  get 'pages/home'
   get 'home', to: 'pages#index'
   get 'pines', to: 'pins#pines'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
