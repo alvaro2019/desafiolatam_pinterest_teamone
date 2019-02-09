40ce6ba961005de85c7a53b8a1282d8ee11c05ce
jzapata18 <john.jzapata18@gmail.com>
2019-02-09 13:06:41 -0300

    actualizacion migraciones

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def home
+  	@pins = Pin.all
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_124610) do
+ActiveRecord::Schema.define(version: 2019_02_09_152611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pins", force: :cascade do |t|
+    t.string "name"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "photo"
+    t.index ["user_id"], name: "index_pins_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_02_09_124610) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pins", "users"
 end
