5485db29be8b07c3a1209a2f531fc277011504c5
jzapata18 <john.jzapata18@gmail.com>
2019-02-16 13:13:25 -0300

    adding show, edit, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   get 'pages/index'
   get 'pages/home'
-  get 'home', to: 'pages#index'
-  get 'pines', to: 'pins#pines'
+
+post 'pins/:id/like', as: 'pins_like', to: 'pins#like'
+
+  #get 'home', to: 'pages#index'
+  #get 'pines', to: 'pins#pines'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  resources :pins
+
+  root 'pins#index'
 end
